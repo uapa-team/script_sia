160f3f25db173c4538b487ad73dc24526609cd55
Laura Molina <ldmolinav@unal.edu.co>
2019-04-08 07:58:29 -0500

    Añadiendo módulo de materias

--- SIARetrieve/sia.py
+++ SIARetrieve/sia.py
@@ -2,6 +2,7 @@ from . import NetworkUtils
 from . import SIAInfo
 from .hist_acad.HistAcad import HistAcad
 from .dato_pers.DatoPers import DatoPers
+from .mate_info.Materia import Materia
 
 def build_data(prog, exped):
     return {'plan':prog, 'expediente':exped}
@@ -16,3 +17,6 @@ def get_ha_per(dni_per):
 
 def get_dp_per(dni_per):
     return DatoPers(NetworkUtils.make_request(SIAInfo.sia_dato_pers, dni_per, None))
+
+def get_mat_info(cod_materia):
+    return Materia(NetworkUtils.make_request_mat(cod_materia))
